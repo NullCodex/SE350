83d951555759d2d3a19236bb32b5378975563b90
arajeev <arajeev@uwaterloo.ca>
2016-02-27 13:12:40 -0500

    added common.h for user_proc.c and handler fns

--- manual_code/Context_Switching_UART/src/rtx.h
+++ manual_code/Context_Switching_UART/src/rtx.h
@@ -30,6 +30,7 @@ typedef struct proc_init
 /* ----- RTX User API ----- */
 #define __SVC_0  __svc_indirect(0)
 
+/* Process Management */ 
 extern void k_rtx_init(void);
 #define rtx_init() _rtx_init((U32)k_rtx_init)
 extern void __SVC_0 _rtx_init(U32 p_func);
@@ -37,10 +38,6 @@ extern void __SVC_0 _rtx_init(U32 p_func);
 extern int k_release_processor(void);
 #define release_processor() _release_processor((U32)k_release_processor)
 extern int __SVC_0 _release_processor(U32 p_func);
-
-extern void *k_request_memory_block(void);
-#define request_memory_block() _request_memory_block((U32)k_request_memory_block)
-extern void *_request_memory_block(U32 p_func) __SVC_0;
 
 extern int k_set_process_priority(int, int);
 #define set_process_priority(process_id, priority) _set_process_priority((U32)k_set_process_priority, process_id, priority)
@@ -51,8 +48,26 @@ extern int k_get_process_priority(int);
 extern int _get_process_priority(U32 p_func, int process_id) __SVC_0;
 /* __SVC_0 can also be put at the end of the function declaration */
 
+/* Memory Management */ 
+extern void *k_request_memory_block(void);
+#define request_memory_block() _request_memory_block((U32)k_request_memory_block)
+extern void *_request_memory_block(U32 p_func) __SVC_0;
+
 extern int k_release_memory_block(void *);
 #define release_memory_block(p_mem_blk) _release_memory_block((U32)k_release_memory_block, p_mem_blk)
 extern int _release_memory_block(U32 p_func, void *p_mem_blk) __SVC_0;
 
+/* IPC Management */
+extern int k_send_message(int pid, void *p_msg);
+#define send_message(pid, p_msg) _send_message((U32)k_send_message, pid, p_msg)
+extern int _send_message(U32 p_func, int pid, void *p_msg) __SVC_0;
+
+extern void *k_receive_message(int *p_pid);
+#define receive_message(p_pid) _receive_message((U32)k_receive_message, p_pid)
+extern void *_receive_message(U32 p_func, void *p_pid) __SVC_0;
+
+/* Timing Service */
+extern int k_delayed_send(int pid, void *p_msg, int delay);
+#define delayed_send(pid, p_msg, delay) _delayed_send((U32)k_delayed_send, pid, p_msg, delay)
+extern int _delayed_send(U32 p_func, int pid, void *p_msg, int delay) __SVC_0;  
 #endif /* !RTX_H_ */
--- manual_code/Context_Switching_UART/src/usr_proc.c
+++ manual_code/Context_Switching_UART/src/usr_proc.c
@@ -21,177 +21,25 @@ void set_test_procs() {
 	int i;
 	for( i = 0; i < NUM_TEST_PROCS; i++ ) {
 		g_test_procs[i].m_pid=(U32)(i+1);
+		g_test_procs[i].m_priority=LOWEST;
 		g_test_procs[i].m_stack_size=0x100;
 	}
   
 	g_test_procs[0].mpf_start_pc = &proc1;
-	g_test_procs[0].m_priority=MEDIUM;
-
 	g_test_procs[1].mpf_start_pc = &proc2;
-	g_test_procs[1].m_priority=MEDIUM;
-	
-	g_test_procs[2].mpf_start_pc = &proc3;
-	g_test_procs[2].m_priority=LOW;
-	
-	g_test_procs[3].mpf_start_pc = &proc4;
-	g_test_procs[3].m_priority=LOW;
-	
-	g_test_procs[4].mpf_start_pc = &proc5;
-	g_test_procs[4].m_priority=LOW;
-	
-	g_test_procs[5].mpf_start_pc = &proc6;
-	g_test_procs[5].m_priority=LOW;
 }
 
 
 /**
- * @brief: a process that prints 2x5 lowercase letters
+ * @brief: a process that prints 5x6 uppercase letters
+ *         and then yields the cpu.
  */
 void proc1(void)
 {
 	int i = 0;
-	int counter = 0;
-	int ret_val = 100;
-	while ( 1 ) {
-		
-		if ( i != 0 && i%5 == 0 ) {
-			uart0_put_string("\n\r");
-			counter++;
-			if ( counter == 2 ) {
-				ret_val = set_process_priority(g_test_procs[1].m_pid , HIGH);
-				break;
-			} else {
-				ret_val = release_processor();
-			}
-#ifdef DEBUG_0
-			printf("proc1: ret_val = %d \n", ret_val);
-#endif /* DEBUG_0 */
-		}
-		uart0_put_char('a' + i%10);
-		i++;
-	}
-	uart0_put_string("proc1 end of testing\n\r");
-	while ( 1 ) {
-		release_processor();
-	}
-}
-
-/*	
- * @brief: a process that prints 4x5 numbers 
- */
-void proc2(void)
-{
-	int i = 0;
-	int ret_val = 20;
-	int counter = 0;
-	
-	while ( 1) {
-		if ( i != 0 && i%5 == 0 ) {
-			uart0_put_string("\n\r");
-			counter++;
-			if ( counter == 4 ) {
-				ret_val = set_process_priority(g_test_procs[0].m_pid , HIGH);
-				break;
-			} else {
-				ret_val = release_processor();
-			}
-#ifdef DEBUG_0
-			printf("proc2: ret_val=%d\n", ret_val);
-#endif /* DEBUG_0 */
-		}
-		uart0_put_char('0' + i%10);
-		i++;
-	}
-	uart0_put_string("proc2 end of testing\n\r");
-	while ( 1 ) {
-		release_processor();
-	}
-}
-
-/**
- * @brief: a process that prints 5x6 `*` character
- *         and then yields the cpu.
- */
-void proc3(void)
-{
-	int i = 0;
-	int counter = 0;
-	int ret_val = 100;
-	while ( 1 ) {
-		
-		if ( i != 0 && i%5 == 0 ) {
-			uart1_put_string("\n\r");
-			counter++;
-			if ( counter == 2 ) {
-				ret_val = set_process_priority(g_test_procs[2].m_pid, LOW);
-				break;
-			} else {
-				ret_val = release_processor();
-			}
-#ifdef DEBUG_0
-			printf("proc3: ret_val = %d \n", ret_val);
-#endif /* DEBUG_0 */
-		}
-		uart1_put_char('a' + i%10);
-		i++;
-	}
-	uart1_put_string("proc3 end of testing\n\r");
-	while ( 1 ) {
-		release_processor();
-	}
-}
-
-/**
- * @brief: a process that prints 5x6 even and odd numbers
- *         and then yields the cpu.
- */
-void proc4(void)
-{
-		int i = 0;
-	int ret_val = 20;
+	int ret_val = 10;
 	int x = 0;
-	void* returnedPtr;
-	int p_priority;
-	int counter = 0;
-	while ( 1) {
-		if ( i != 0 && i%5 == 0 ) {
-			uart1_put_string("\n\r");
-			//returnedPtr = request_memory_block();
-			p_priority = get_process_priority(g_test_procs[2].m_pid);
-			if ( i%30 == 0 ) {
-				//set
-				uart1_put_string("\n\r");
-				counter++;
-				if ( counter == 2 ) {
-					ret_val = set_process_priority(g_test_procs[0].m_pid, HIGH);
-				//	break;
-				} else {
-					ret_val = release_processor();
-				}
-#ifdef DEBUG_0
-				printf("proc4: ret_val=%d\n", ret_val);
-			
-#endif /* DEBUG_0 */
-			}
- 			for ( x = 0; x < 500000; x++); // some artifical delay
-		}
-		uart1_put_char('x');
-		i++;
-	}
-	while(1) {
-		release_processor();
-	}
-}
 
-/**
- * @brief: a process that prints 5x6 even and odd numbers
- *         and then yields the cpu.
- */
-void proc5(void)
-{
-	int i = 0;
-	int ret_val = 20;
-	int x = 0;
 	while ( 1) {
 		if ( i != 0 && i%5 == 0 ) {
 			uart1_put_string("\n\r");
@@ -199,50 +47,42 @@ void proc5(void)
 			if ( i%30 == 0 ) {
 				ret_val = release_processor();
 #ifdef DEBUG_0
-				printf("proc5: ret_val=%d\n", ret_val);
+				printf("proc1: ret_val=%d\n", ret_val);
 			
 #endif /* DEBUG_0 */
 			}
 			for ( x = 0; x < 500000; x++); // some artifical delay
 		}
-		uart1_put_char('&');
+		uart1_put_char('A' + i%26);
 		i++;
+		
 	}
 }
 
 /**
- * @brief: a process that prints 5x6 even and odd numbers
+ * @brief: a process that prints 5x6 numbers
  *         and then yields the cpu.
  */
-void proc6(void)
+void proc2(void)
 {
 	int i = 0;
 	int ret_val = 20;
 	int x = 0;
-	void* returnedPtr;
-	int p_priority;
-	int count = 1;
 	while ( 1) {
 		if ( i != 0 && i%5 == 0 ) {
 			uart1_put_string("\n\r");
-			//returnedPtr = request_memory_block();
+			
 			if ( i%30 == 0 ) {
 				ret_val = release_processor();
-				p_priority = get_process_priority(count++);
-				uart1_put_string("received priority:\n");
-				uart1_put_char('0' + p_priority%10);
-				if(count > NUM_TEST_PROCS) {
-					count = 1;
-				}
 #ifdef DEBUG_0
-				printf("proc6: ret_val=%d\n", ret_val);
+				printf("proc2: ret_val=%d\n", ret_val);
 			
 #endif /* DEBUG_0 */
 			}
- 			for ( x = 0; x < 500000; x++); // some artifical delay
+			for ( x = 0; x < 500000; x++); // some artifical delay
 		}
-		uart1_put_char('$');
+		uart1_put_char('0' + i%10);
 		i++;
+		
 	}
 }
-
