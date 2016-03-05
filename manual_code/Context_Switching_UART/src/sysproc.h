4eda187def5b6b777a95a2344ab39803da6bb0a5
unknown <xz2yu@ece-rtos12.NEXUS.UWATERLOO.CA>
2016-03-05 15:58:01 -0500

    Fixed sys proc compile problem

--- manual_code/Context_Switching_UART/src/sysproc.h
+++ manual_code/Context_Switching_UART/src/sysproc.h
@@ -1,9 +1,13 @@
 #ifndef SYS_PROC_H_
 #define SYS_PROC_H_
 
+#include "k_rtx.h"
+#include "msg.h"
+#include "common.h"
 
 void crt_proc(void);
 void UART_iprocess(void);
 void kcd_proc(void);
-
+void print_wall_clock(int, int, int);
+void wall_clock(void);
 #endif
